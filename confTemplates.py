80436b4de34112444f5223de002d3ba2af08c76e
Knau <knuudan1993@live.com>
2019-04-17 17:26:40 +0200

    Update confTemplates.py

--- confTemplates.py
+++ confTemplates.py
@@ -21,3 +21,15 @@ def service_security():
 		sshshell.send('no ip icmp redirect\n')
 		sshshell.send('exit\n')
 		time.sleep(1) #Wait for buffer
+
+def switch_interfaceConf():
+        switchPrompt = input("Wich interfaces do you want to shut down? ")
+        switchINT = ('interface range') +  switchPrompt
+        print("The interfaces shutting down is: " + switchPrompt)
+        
+        sshshell.send('switchINT\n')
+        sshshell.send('shutdown\n')
+        sshshell.send('exit\n')
+        time.sleep(1) #Wait to buffer
+
+
