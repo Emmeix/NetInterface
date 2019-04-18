4c825a7cdb04fa6b439fd99e5cc65e6481ee6c97
Knau <knuudan1993@live.com>
2019-04-18 11:26:30 +0200

    Update confTemplates.py

--- confTemplates.py
+++ confTemplates.py
@@ -32,4 +32,30 @@ def switch_interfaceConf():
         sshshell.send('exit\n')
         time.sleep(1) #Wait to buffer
 
+def switch_interfaceConf():
+            switchSec = input("Wich interface do you want to secure? ")
+            switchCom = ('interface ') + switchSec
+            sshshell.send(switchCom)
+            sshshell.send('\n')
+            sshshell.send('switchport mode access\n')
+            sshshell.send('switchport port-security\n')
+            sshshell.send('exit\n')
+            switchMAC = input("Set MAC security to sticky Y/N? ")
+            if switchMAC == "y":
+                sshshell.send('switchport port-security\n')
+                sshshell.send('switchport port-security mac-address sticky\n')
+                sshshell.send('switchport port-security maximum 2\n')
+            if switchMAC == "n":
+                return
+
+def switch_interfaceSHUT():
+                switchPrompt = input("Wich interfaces do you want to shut down? F0/1-24 & G0/1-2 is available: ")
+                switchINT = ('interface range ') +  switchPrompt
+                print("The interfaces shutting down is: " + switchPrompt)        
+                sshshell.send('\n')
+                sshshell.send(switchINT)
+                sshshell.send('\n')
+                sshshell.send('shutdown\n')
+                time.sleep(1) #Wait to buffer
+
 
